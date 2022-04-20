cc6acac69fbf3e4054f3c4a5e134bb7feb1243e1
Vorosath <35424590+Vorosath@users.noreply.github.com>
2022-04-20 04:42:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,5 @@
 #include "fun.h"
 
 int main() {
-    const char* str = "check proverka hello ";
-    unsigned int result1 = faStr1(str);
-    unsigned int result2 = faStr2(str);
-    unsigned int result3 = faStr3(str);
-    std::cout << result1 << std::endl;
-    std::cout << result2 << std::endl;
-    std::cout << result3 << std::endl;
+    return 0;
 }
